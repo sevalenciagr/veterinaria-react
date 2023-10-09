7333446925c7f18c120177f8255cc478bd365f99
Sebastián Valencia G <sevalenciagr@gmail.com>
2023-10-09 14:41:52 -0500

    Creando app

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import Form from "./components/Form"
+import Header from "./components/Header"
+import PatientList from "./components/PatientList"
+
+
+function App() {
+
+  return (
+    <div className='container mx-auto mt-20'>
+      <Header />
+
+      <div className="mt-12 md:flex ">
+        <Form />  
+        <PatientList />
+      </div> 
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/PatientList.jsx
@@ -0,0 +1,13 @@
+
+
+const PatientList = () => {
+  return (
+    
+    <div>
+        <h2 className="w-1/2 lg:w-3/5">Listado Pacientes</h2>
+    </div>
+    
+  )
+}
+
+export default PatientList
