bd4a6d6ac5ee5668cde4bd23e3678699f3d69244
Sebastián Valencia G <sevalenciagr@gmail.com>
2023-10-10 14:49:48 -0500

    Edit component Form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import {useState, useEffect} from 'react';
 
 const Form = () => {
+
+  const [nombre, setNombre] = useState('');
+  const [propietario, setPropietario] = useState('');
+  const [email, setEmail] = useState('');
+  const [fecha, setFecha] = useState('');
+  const [sintomas, setSintomas] = useState('');
+
+  const [error, setError] = useState(false);
+
+  
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // Validación del Form
+    if([ nombre, propietario, email, fecha, sintomas ].includes('')){
+      console.log('Hay al menos un campo vacío');
+
+      setError(true);
+    } else {
+      console.log('Todos los campos estan completos');
+    }    
+  };
+
   return (
-    <div className="md:w-1/2 lg:w-2/5">
+    <div className="md:w-1/2 lg:w-2/5 mx-5">
         <h2 className='font-black text-3xl text-center'>Seguimiento Pacientes</h2>
 
         <p className='text-lg mt-5 text-center mb-10'>
@@ -10,7 +33,10 @@ const Form = () => {
             <span className='text-indigo-600 font-bold '>Administralos</span>
         </p>
 
-        <form className='bg-white shadow-md rounded-lg py-10 px-5 mb-10'>
+        <form 
+          onSubmit={handleSubmit}
+          className='bg-white shadow-md rounded-lg py-10 px-5 mb-10 '>
+            { error && (<div><p>Hay un error</p></div>) }
             <div className='mb-5'>
                 <label htmlFor='mascota' className='block text-gray-700 uppercase font-bold'>
                   Nombre Mascota
@@ -21,6 +47,8 @@ const Form = () => {
                 type="text"
                 placeholder='Nombre de la Mascota'
                 className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md'
+                value={nombre}
+                onChange={ (e) => setNombre(e.target.value) }
                 />
             </div>
 
@@ -34,6 +62,8 @@ const Form = () => {
                 type="text"
                 placeholder='Nombre del Propietario'
                 className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md'
+                value={propietario}
+                onChange={ (e) => setPropietario(e.target.value) }
                 />
             </div>
 
@@ -47,6 +77,8 @@ const Form = () => {
                 type="email"
                 placeholder='Email'
                 className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md'
+                value={email}
+                onChange={ (e) => setEmail(e.target.value) }
                 />
             </div>
 
@@ -59,6 +91,8 @@ const Form = () => {
                 id='alta'
                 type="date"
                 className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md'
+                value={fecha}
+                onChange={ (e) => setFecha(e.target.value) }
                 />
             </div>
 
@@ -72,6 +106,8 @@ const Form = () => {
                   id="sintomas" 
                   className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md'
                   placeholder='Describe los síntomas'
+                  value={sintomas}
+                  onChange={ (e) => setSintomas(e.target.value) }
                 />
             </div>
             
--- src/components/PatientList.jsx
+++ src/components/PatientList.jsx
@@ -4,7 +4,7 @@ import Patient from "./Patient"
 const PatientList = () => {
   return (
     
-    <div className="md:w-1/2 lg:w-3/5 md:h-screen overflow-y-scroll">
+    <div className="md:w-1/2 lg:w-3/5 md:h-screen overflow-y-scroll mx-5">
         <h2 className="font-black text-3xl text-center">Listado Pacientes</h2>
         <p className='text-lg mt-5 text-center mb-10'>
           Administra tus {' '}
