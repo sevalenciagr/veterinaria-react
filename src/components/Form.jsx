0212d9c938cec4e11c921c957cf7168b6ee30231
Sebastián Valencia G <sevalenciagr@gmail.com>
2023-10-10 08:57:08 -0500

    Changes in Form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -10,9 +10,11 @@ const Form = () => {
             <span className='text-indigo-600 font-bold '>Administralos</span>
         </p>
 
-        <form className='bg-white shadow-md rounded-lg py-10 px-5'>
-            <div>
-                <label htmlFor='mascota' className='block text-gray-700 uppercase font-bold'>Nombre Mascota</label>
+        <form className='bg-white shadow-md rounded-lg py-10 px-5 mb-10'>
+            <div className='mb-5'>
+                <label htmlFor='mascota' className='block text-gray-700 uppercase font-bold'>
+                  Nombre Mascota
+                </label>
 
                 <input 
                 id='mascota'
@@ -21,6 +23,63 @@ const Form = () => {
                 className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md'
                 />
             </div>
+
+             <div className='mb-5'>
+                <label htmlFor='propietario' className='block text-gray-700 uppercase font-bold'>
+                  Nombre Propietario
+                </label>
+
+                <input 
+                id='propietario'
+                type="text"
+                placeholder='Nombre del Propietario'
+                className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md'
+                />
+            </div>
+
+            <div className='mb-5'>
+                <label htmlFor='email' className='block text-gray-700 uppercase font-bold'>
+                  Email
+                </label>
+
+                <input 
+                id='email'
+                type="email"
+                placeholder='Email'
+                className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md'
+                />
+            </div>
+
+            <div className='mb-5'>
+                <label htmlFor='email' className='block text-gray-700 uppercase font-bold'>
+                  Alta
+                </label>
+
+                <input 
+                id='alta'
+                type="date"
+                className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md'
+                />
+            </div>
+
+            <div className='mb-5'>
+                <label htmlFor='email' className='block text-gray-700 uppercase font-bold'>
+                  Síntomas
+                </label>
+
+                <textarea 
+                  name="" 
+                  id="sintomas" 
+                  className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md'
+                  placeholder='Describe los síntomas'
+                />
+            </div>
+            
+            <input 
+              type="submit" 
+              className='bg-indigo-600 w-full p-3 text-white uppercase font-bold hover:bg-indigo-700 cursor-pointer transition-all'
+              value='Agregar paciente'
+            />
         </form>
     </div>
    
