1f21d495c10f77788eb821cc2f24a2379612a4ae
Sebastián Valencia G <sevalenciagr@gmail.com>
2023-10-10 09:39:16 -0500

    Create component Patient

---
+++ src/components/Patient.jsx
@@ -0,0 +1,29 @@
+
+
+const Patient = () => {
+    return (
+        <div className="m-3 bg-white shadow-md px-5 py-10 rounded-xl">
+            <p className="font-bold mb-3 text-gray-700 uppercase">Nombre:{' '}
+                <span className="font-normal normal-case">Hook</span>
+            </p>
+
+            <p className="font-bold mb-3 text-gray-700 uppercase">Propietario:{' '}
+                <span className="font-normal normal-case">Hook</span>
+            </p>
+
+            <p className="font-bold mb-3 text-gray-700 uppercase">Email:{' '}
+                <span className="font-normal normal-case">Hook</span>
+            </p>
+
+            <p className="font-bold mb-3 text-gray-700 uppercase">Fecha de alta:{' '}
+                <span className="font-normal normal-case">Hook</span>
+            </p>
+
+            <p className="font-bold mb-3 text-gray-700 uppercase">Fecha de alta:{' '}
+                <span className="font-normal normal-case">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi quia accusantium alias obcaecati recusandae optio, praesentium impedit, iure et dicta, neque ipsum mollitia veritatis inventore. Animi explicabo repudiandae atque repellendus!</span>
+            </p>
+        </div>
+    )
+}
+
+export default Patient
